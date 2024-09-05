94223e1382bde51dcf23b9484cc88b0b34dddfac
Luke O <luke.oakden02@outlook.com>
2024-09-06 09:52:04 +1000

    real-time speech recognition with low latency using torch and numpy handling of audio stream

--- speech.py
+++ speech.py
@@ -1,56 +1,102 @@
-from threading import Thread
-from queue import Queue
-from deep_translator import GoogleTranslator
+import os
+import time
+import datetime
+import sys
+import os
+import torch
+import whisper
+import numpy as np
 import speech_recognition as sr
-
-r = sr.Recognizer()
-audio_queue = Queue()
+from queue import Queue
 
 
-def speech_recognition():
-    # this runs in a background thread
-    while True:
-        audio = audio_queue.get()  # retrieve the next audio processing job from the main thread
-        if audio is None:
-            break  # stop processing if the main thread is done
+class Lumino:
+    def main(self):
+        # Time when a spoken line was taken fom queue
+        line_time = None
 
-        # received audio data, now we'll recognize it using Google Speech Recognition
-        try:
-            # for testing purposes, we're just using the default API key
-            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-            # instead of `r.recognize_google(audio)`
-            text = r.recognize_whisper(audio)
-            print("Decoded Text: {}".format(text))
-            print(translate(text=text))
+        audio_queue = Queue()
 
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-        except sr.RequestError as e:
-            print("Request results error")
+        recognizer = sr.Recognizer()
+        recognizer.dynamic_energy_threshold = False
 
-        audio_queue.task_done()  # mark the audio processing job as completed in the queue
+        # common linux bug
+        source = None
+        if 'linux' in sys.platform:
+            mic_name = "pulse"
+            for i, mic in enumerate(sr.Microphone.list_microphone_names()):
+                if mic_name in mic:
+                    source = sr.Microphone(sample_rate=16000, device_index=i)
+                    break
+        else:
+            source = sr.Microphone()
 
+        # Load / Download model
+        audio_model = whisper.load_model("small")
 
-def translate(source='en', target='zh-CN', text=''):
-    # https://pypi.org/project/deep-translator/#google-translate-1
-    return GoogleTranslator(source=source, target=target).translate(text)
+        # How real time the recording is in seconds.
+        record_timeout = 2
+        # How much empty space between recordings before we consider it a new line in the transcription.
+        line_timeout = 3
 
+        speech_text = ['']
 
-# start a new thread to recognize audio, while this thread focuses on listening
-recognize_thread = Thread(target=speech_recognition)
-recognize_thread.daemon = True
-recognize_thread.start()
-with sr.Microphone() as source:
-    try:
         print("Adjusting noise...")
-        r.adjust_for_ambient_noise(source, duration=1)
-        print("Recording...")
-        while True:  # repeatedly listen for phrases and put the resulting audio on the audio processing job queue
-            audio_queue.put(r.listen(source))
-    except KeyboardInterrupt:  # allow Ctrl + C to shut down the program
-        print("Interrupted")
-        pass
+        with source:
+            recognizer.adjust_for_ambient_noise(source, duration=1)
 
-audio_queue.join()  # block until all current audio processing jobs are done
-audio_queue.put(None)  # tell the recognize_thread to stop
-recognize_thread.join()  # wait for the recognize_thread to actually stop
+        def recognise(r, audio_data: sr.AudioData):
+            """
+            Get the audio data line-by-line and add to queue
+            :param audio_data: incoming audio stream
+            :return:
+            """
+            data = audio_data.get_raw_data()
+            audio_queue.put(data)
+
+        recognizer.listen_in_background(source, recognise, phrase_time_limit=line_timeout)
+
+        # audio data in bytes
+        audio_data = b''
+
+        while True:
+            try:
+                start = time.time()
+
+                if not audio_queue.empty():
+                    line_end = False
+                    # line ends when 3s has passed
+                    if time.time() - start > line_timeout:
+                        line_end = True
+                        # new audio data -> new line
+                        audio_data = b''
+                    line_time = start
+
+                    # combine audio data
+                    audio_data = audio_data + b''.join(audio_queue.queue)
+                    audio_queue.queue.clear()
+
+                    # numpy torch audio data stuff
+                    audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
+
+                    # Read the transcription.
+                    result = audio_model.transcribe(audio_np, fp16=torch.cuda.is_available())
+                    text = result['text'].strip()
+
+                    # either add new line or edit last line
+                    if line_end:
+                        speech_text.append(text)
+                    else:
+                        speech_text[-1] = text
+
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    for line in speech_text:
+                        print(line)
+                    print('', end='', flush=True)
+            except KeyboardInterrupt:
+                break
+
+
+if __name__ == "__main__":
+    lum = Lumino()
+    lum.main()
