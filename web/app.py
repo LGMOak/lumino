d3ae104f86e97da1c7ddd5d083e0bb9548d847a0
Yoimiyabestgirl <129245993+Yoimiyabestgirl@users.noreply.github.com>
2024-09-10 21:31:59 +1000

    Update app.py
    Response with required data from lumino.py

--- web/app.py
+++ web/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3
-import lumino
+from lumino import Lumino
+import time
 import sys
+from threading import Thread, Event
+import queue
 
 app = Flask(__name__)
-
+lumino = Lumino()
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -14,11 +17,11 @@ def get_db_connection():
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    info = conn.execute('SELECT * FROM info').fetchall()
-    conn.close()
-    speech_txt = lumino.Lumino()
-    return render_template('index.html', info=info)
+    #conn = get_db_connection()
+    #info = conn.execute('SELECT * FROM info').fetchall()
+    #conn.close()
+    #speech_txt = lumino.Lumino()
+    return render_template('index.html')
 
 
 @app.route('/about')
@@ -31,19 +34,39 @@ def hello():
     name = request.form['name']
     return render_template('hello.html', name=name)
 
-# API endpoint to return translated  text
+
+
+def run_speech_to_text(lumino):
+    lumino.speech_to_text()  
+
 @app.route('/translate', methods=['POST'])
 def translate():
     lumino = Lumino()
-    recognized_text = lumino.speech_recog()
+    # New thread to run speech_to_text
+    thread = Thread(target=run_speech_to_text, args=(lumino,))
+    thread.start()
+    thread.join(timeout=15)  # wait for 15 seconds for the thread to finish, can be adjusted
 
-    if recognized_text and recognized_text.strip():
+    if lumino.speech_text:
+        # If there is a recognized text
+        recognized_text = ''.join(lumino.speech_text)
         translated_text = lumino.translate(text=recognized_text)
-        # Return a JSON response 
-        return jsonify(success=True, translation=translated_text)
+        response_data = {
+            'success': True,
+            'recognized_text': recognized_text,
+            'translated_text': translated_text
+        }
     else:
-        return jsonify(success=False, error="Speech recognition failed")
+        # If no speech detected or timeout
+        response_data = {
+            'success': False,
+            'error': 'Timeout or no speech detected'
+        }
+
+    return jsonify(response_data)
 
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run()
+
