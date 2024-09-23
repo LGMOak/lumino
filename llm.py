30bcddea4c3396004151c146fceb91f42170709e
Luke O <luke.oakden02@outlook.com>
2024-09-23 11:04:37 +1000

    Testing LLM generation capabilities for other languages

--- llm.py
+++ llm.py
@@ -4,7 +4,8 @@ from keys import GEMINI_API_KEY
 gem.configure(api_key=GEMINI_API_KEY)
 
 model = gem.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain quantum mechanics at 1st year undergraduate level.", stream=True)
+response = model.generate_content("Explain basic machine learning models"
+                                  "at an undergraduate level in simple Chinese.", stream=True)
 
 for chunk in response:
     print(chunk.text)
