b747a4947d38ee8162b433e3a3ca9879e69e6387
Yoimiyabestgirl <129245993+Yoimiyabestgirl@users.noreply.github.com>
2024-10-17 15:19:56 +1000

    Update comments and docstrings

--- web/lumino.py
+++ web/lumino.py
@@ -12,49 +12,92 @@ from keys import GEMINI_API_KEY, DEEPL_API_KEY
 import threading
 
 class Lumino:
+    """
+    The Lumino class manages speech recognition, translation, and context generation.
+    This class includes microphone initialization, audio processing, translation functionality, and generating conversation context.
+    """
+
     def __init__(self):
-        self.DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")
-        self.spoken_language = "EN"
-        self.target_language = "ZH"
-        self.scenarios = {"General": "This context refers to everyday, general conversations without specific focus that a elderly Chinese immigrant in Australia may hav. It covers casual interactions that are not tied to any particular domain, such as greetings, basic questions, or social small talk. In this context, the conversation could involve a variety of topics ranging from weather, personal well-being, or simple inquiries. The goal is to keep the translation as neutral and everyday as possible, avoiding any specialized terminology.",
-                          "Local Community": "This context relates to conversations within the local community, such as interactions with family members, friends, neighbors, tradespeople, shop owners, cashiers, and other people you encounter regularly in your day-to-day life. This includes small transactions, informal exchanges, asking for help, or engaging with people in familiar settings. The language used here is more informal, reflecting the tone of a community that is close-knit and casual.",
-                          "Socialservices": "This context involves conversations related to government services and welfare programs in Australia, such as Centrelink (financial assistance), Medicare (public healthcare), superannuation (retirement savings), pensions, taxes, and other services that assist in everyday living. It includes discussions about applying for benefits, understanding rights and entitlements, navigating bureaucracy, and engaging with service providers. The language should be clear, straightforward, and possibly include formal or legal terminology often used in public services.",
-                          "Medical Services": "This context focuses on medical-related conversations, including interactions with healthcare professionals like general practitioners (GPs), hospitals, physiotherapists, pharmacists, and nurses. It covers discussions about appointments, medical treatments, prescriptions, health advice, and explanations of medical conditions. In this context, the language is both technical (with medical terminology) and conversational, reflecting the seriousness of healthcare while remaining accessible for patients, especially those unfamiliar with medical terms. Understanding the correct use of terminology is key to avoid confusion."}
-        self.selected_scenario = "General"
+        """
+        Initialize an instance of the Lumino class, including setting language, context, microphone source, and speech recognition model.
+        """
+        self.DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")  # Get Deepl API key
+        self.spoken_language = "EN"  # Default spoken language
+        self.target_language = "ZH"  # Default target language
+        # Scenarios and their descriptions used to generate context
+        self.scenarios = {
+            "General": "This context refers to everyday, general conversations without specific focus that an elderly Chinese immigrant in Australia may have. It covers casual interactions that are not tied to any particular domain, such as greetings, basic questions, or social small talk. In this context, the conversation could involve a variety of topics ranging from weather, personal well-being, or simple inquiries. The goal is to keep the translation as neutral and everyday as possible, avoiding any specialized terminology.",
+            "Local Community": "This context relates to conversations within the local community, such as interactions with family members, friends, neighbors, tradespeople, shop owners, cashiers, and other people you encounter regularly in your day-to-day life. This includes small transactions, informal exchanges, asking for help, or engaging with people in familiar settings. The language used here is more informal, reflecting the tone of a community that is close-knit and casual.",
+            "Socialservices": "This context involves conversations related to government services and welfare programs in Australia, such as Centrelink (financial assistance), Medicare (public healthcare), superannuation (retirement savings), pensions, taxes, and other services that assist in everyday living. It includes discussions about applying for benefits, understanding rights and entitlements, navigating bureaucracy, and engaging with service providers. The language should be clear, straightforward, and possibly include formal or legal terminology often used in public services.",
+            "Medical Services": "This context focuses on medical-related conversations, including interactions with healthcare professionals like general practitioners (GPs), hospitals, physiotherapists, pharmacists, and nurses. It covers discussions about appointments, medical treatments, prescriptions, health advice, and explanations of medical conditions. In this context, the language is both technical (with medical terminology) and conversational, reflecting the seriousness of healthcare while remaining accessible for patients, especially those unfamiliar with medical terms. Understanding the correct use of terminology is key to avoid confusion."
+        }
+        self.selected_scenario = "General"  # Default scenario
         self.line_time = None
-        self.audio_queue = Queue()
-        self.recognizer = sr.Recognizer()
-        self.recognizer.dynamic_energy_threshold = False
-        gem.configure(api_key=GEMINI_API_KEY)
-        self.model = gem.GenerativeModel("gemini-1.5-flash")
-        self.source = None
-        self.audio_model = whisper.load_model("small")
-        self.record_timeout = 2
-        self.line_timeout = 3
-        self.speech_text = ['']
-        self.spoken_line = ""
-        self.stop_event = threading.Event()
-        self.stop_listening = None
+        self.audio_queue = Queue()  # Queue for storing audio data
+        self.recognizer = sr.Recognizer()  # Initialize speech recognizer
+        self.recognizer.dynamic_energy_threshold = False  # Disable dynamic energy threshold
+        gem.configure(api_key=GEMINI_API_KEY)  # Configure generative AI API key
+        self.model = gem.GenerativeModel("gemini-1.5-flash")  # Initialize generative AI model
+        self.source = None  # Microphone source
+        self.audio_model = whisper.load_model("small")  # Load Whisper speech recognition model
+        self.record_timeout = 2  # Recording timeout
+        self.line_timeout = 3  # Line timeout
+        self.speech_text = ['']  # Store the entire conversation text
+        self.spoken_line = ""  # Current spoken line
+        self.stop_event = threading.Event()  # Event to stop speech recognition
+        self.stop_listening = None  # Save the stop listening function
 
     def translate(self, source='EN', target='ZH-HANS', text=''):
+        """
+        Translate text using Deepl API.
+        
+        Args:
+            source (str): Source language code.
+            target (str): Target language code.
+            text (str): Text to be translated.
+        
+        Returns:
+            str: Translated text.
+        """
         translator = deepl.Translator(DEEPL_API_KEY)
         formality = 'prefer_less' if self.get_context() not in ["Medical", "Services"] else 'prefer_more'
         return translator.translate_text(text=text, source_lang=source, target_lang=target,
                                          context=self.get_scenarios()[self.get_context()], formality=formality).text
 
     def set_input_source(self, input_device):
+        """
+        Set the input audio source (microphone).
+        
+        Args:
+            input_device (int): Index of the input device.
+        """
         try:
             self.source = sr.Microphone(input_device, sample_rate=16000)
-            print(f"Microphone set to: {self.source}")  # Log the microphone setup
+            print(f"Microphone set to: {self.source}")  # Log microphone setup information
         except Exception as e:
             print(f"Error setting microphone source: {e}")
-            self.source = sr.Microphone(sample_rate=16000)
+            self.source = sr.Microphone(sample_rate=16000)  # Set default microphone
 
     def set_language(self, speaking_language):
+        """
+        Set the spoken and target languages.
+        
+        Args:
+            speaking_language (str): Spoken language.
+        """
         self.target_language = self.spoken_language
         self.spoken_language = speaking_language
 
     def generate_context(self, prompt=""):
+        """
+        Generate conversation context based on the current scenario and previous conversation.
+        
+        Args:
+            prompt (str): Current conversation text.
+        
+        Returns:
+            str: Generated context text.
+        """
         context_prompt = (f"Explain this conversation for me so far in as few sentences as possible: {prompt}."
                           f"Try to infer the context of the conversation, where the scenario is {self.get_scenarios()[self.get_context()]}"
                           f" given all the previous lines. Give the entire output in {self.target_language} language")
@@ -62,19 +105,43 @@ class Lumino:
         return response.text
 
     def get_context(self):
+        """
+        Get the current conversation scenario.
+        
+        Returns:
+            str: The currently selected scenario.
+        """
         return self.selected_scenario
 
     def set_context(self, context_scenario):
+        """
+        Set the conversation scenario.
+        
+        Args:
+            context_scenario (str): Selected scenario.
+        """
         self.selected_scenario = context_scenario
 
     def get_scenarios(self):
+        """
+        Get all available scenarios.
+        
+        Returns:
+            dict: All available scenarios and their descriptions.
+        """
         return self.scenarios
 
     def reset_stop_event(self):
+        """
+        Reset the stop event.
+        """
         self.stop_event.clear()
         self.stop_listening = None
 
     def stop_recognition(self):
+        """
+        Stop the speech recognition process and clear the audio queue.
+        """
         self.stop_event.set()
         if self.stop_listening is not None:
             self.stop_listening()
@@ -83,12 +150,18 @@ class Lumino:
 
     def clear_conversation(self):
         """
-        Clear the conversation history for a new session.
+        Clear the current conversation history for a new session.
         """
         self.speech_text = ['']
         self.spoken_line = ""
 
     def speech_recognition(self):
+        """
+        Perform speech recognition, generate recognized text and translated content.
+        
+        Yields:
+            tuple: Contains recognized text, translated text, and generated context.
+        """
         print("Setting up microphone")
         if self.source is None:
             self.source = sr.Microphone(sample_rate=16000)
@@ -106,7 +179,7 @@ class Lumino:
             self.audio_queue.put(data)
 
         print("Recording...")
-        # save the stop function
+        # Save the stop function
         self.stop_listening = self.recognizer.listen_in_background(self.source, transcript_data, phrase_time_limit=self.record_timeout)
         audio_data = b''
 
@@ -128,9 +201,8 @@ class Lumino:
                     else:
                         translation = self.translate(text=self.spoken_line)
                     context = self.generate_context(self.spoken_line)
-                    yield self.spoken_line, translation, context
 
-                     # Print recognized and translated text to the console
+                    # Print recognized and translated text to the console
                     print(f"Recognized Text: {text}")
                     print(f"Translated Text: {translation}")
                     yield self.spoken_line, translation, context
