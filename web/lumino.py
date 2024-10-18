0054a3f49aebc48948bdc6e09b981ede21a2cc33
lgmoak <luke.oakden02@outlook.com>
2024-10-18 11:32:21 +1000

    Changed mic index used by default

--- web/lumino.py
+++ web/lumino.py
@@ -164,7 +164,7 @@ class Lumino:
         """
         print("Setting up microphone")
         if self.source is None:
-            self.source = sr.Microphone(14, sample_rate=16000)
+            self.source = sr.Microphone(sample_rate=16000)
 
         with self.source as source:
             try:
